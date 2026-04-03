ed66e3ed1130ad59fd6ecbe439aae7c52024be2a
Prathina07 <prathinaputta07@gmail.com>
2026-04-03 09:59:19 +0530

    fixed cors

--- src/main/java/com/prathina/money_manager/controller/AuthController.java
+++ src/main/java/com/prathina/money_manager/controller/AuthController.java
@@ -1,14 +1,16 @@
 package com.prathina.money_manager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 import com.prathina.money_manager.model.User;
 import com.prathina.money_manager.repository.UserRepository;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     @Autowired
@@ -19,17 +21,24 @@ public class AuthController {
     public User register(@RequestBody User user) {
         return userRepository.save(user);
     }
+    @GetMapping("/users")
+public List<User> getAllUsers() {
+    return userRepository.findAll();
+}
 
-    // LOGIN
-    @PostMapping("/login")
-    public User login(@RequestBody User user) {
+@PostMapping("/login")
+public ResponseEntity<?> login(@RequestBody User user) {
 
-        User existingUser = userRepository.findByUsername(user.getUsername());
+    User existingUser = userRepository.findByUsername(user.getUsername());
 
-        if (existingUser != null && existingUser.getPassword().equals(user.getPassword())) {
-            return existingUser;
-        }
-
-        return null;
+    if (existingUser == null) {
+        return ResponseEntity.status(401).body("User not found");
     }
+
+    if (!existingUser.getPassword().equals(user.getPassword())) {
+        return ResponseEntity.status(401).body("Wrong password");
+    }
+
+    return ResponseEntity.ok(existingUser);
+}
 }
