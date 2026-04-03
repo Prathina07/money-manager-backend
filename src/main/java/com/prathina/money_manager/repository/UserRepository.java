dc09339dafa02d7ae741626bf1828530a36b5524
Prathina07 <prathinaputta07@gmail.com>
2026-04-03 13:40:50 +0530

    fixed missing List import

--- src/main/java/com/prathina/money_manager/repository/UserRepository.java
+++ src/main/java/com/prathina/money_manager/repository/UserRepository.java
@@ -2,7 +2,8 @@ package com.prathina.money_manager.repository;
 
 import com.prathina.money_manager.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;   // ✅ ADD THIS
 
 public interface UserRepository extends JpaRepository<User, Long> {
-  List<User> findByUsername(String username);
+    List<User> findByUsername(String username);
 }
